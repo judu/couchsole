106f96180a926edd0edaca15a012ca3f98e0e6ff
Julien Durillon <julien.durillon@gmail.com>
2013-08-13 01:23:00 +0200

    Make it work with js

--- public/javascripts/documents.js
+++ public/javascripts/documents.js
@@ -1,5 +1,5 @@
 function loadDocs(qty,page) {
-	$.getJson(
+	$.getJSON(
 		'/documents',
 		{
 			quantity: qty || 10,
@@ -10,16 +10,18 @@ function loadDocs(qty,page) {
 			$.each(data, function(key,val) {
 				var line = $('<tr></tr>');
 				var th = $('<th>'+val.id+'</th>');
-				var data = (typeof(val.type) !== 'undefined' && val.type === 'json') ? data.substring(0,data.length > 140 ? 140, data.length) : "&lt;binary data&gt;";
-				var tddata = $('<td>'+data+'</td>');
+				var obj = (typeof(val.type) !== 'undefined' && val.type === 'json') ? val.doc.substring(0,val.doc.length > 140 ? 140 : val.doc.length) : "&lt;binary data&gt;";
+				var tddata = $('<td>'+obj+'</td>');
 				var buttons = $('<td><button class="btn">Edit</button><button class="btn">Delete</button></td>');
-				line.push(th);
-				line.push(tddata);
-				line.push(buttons);
-				table.push(line);
+				line.append(th);
+				line.append(tddata);
+				line.append(buttons);
+				table.append(line);
 			});
-		});
+		}
+	);
 }
+console.log("coucou");
 
 $(document).ready(function() {
 	loadDocs(10,0);
